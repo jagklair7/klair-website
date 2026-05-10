c7d1d2c8d270da0adf8136bb21f47ba77db93e3b
jagklair7 <jagklair7@gmail.com>
2026-05-10 14:59:50 -0600

    Login.jax updated to LiveMonitoring

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -20,7 +20,7 @@ export default function Login() {
       setError(error.message)
       setLoading(false)
     } else {
-      navigate('/dashboard')
+      navigate('/livemonitoring')
     }
   }
 
